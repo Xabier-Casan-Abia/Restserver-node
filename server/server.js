0714626d9578b3fc69bca7ed8fb5831ecc8153d8
DarkScarbo <xabier_abia@hotmail.com>
2019-10-29 14:30:51 +0000

    POST

--- server/server.js
+++ server/server.js
@@ -14,6 +14,20 @@ app.get("/user", function(req, res) {
   res.json("GET User");
 });
 
+app.post("/user", function(req, res) {
+  let body = req.body;
+  if (!body.name) {
+    res.status(400).json({
+      ok: false,
+      message: "Name is required"
+    });
+  } else {
+    res.json({
+      body
+    });
+  }
+});
+
 app.listen(process.env.PORT, () => {
   console.log("Listening from port: ", process.env.PORT);
 });
