67bede47cb18ef2cd65493aa0e59823d73e4b359
Xabier <xabier.casan@gmail.com>
2020-06-02 16:24:12 +0100

    MONGO_URL enviroment variable

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://DarkScarbo:aPe7G6Y+dAJ5tg!@cluster0-iwvgy.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URL;
 }
 
 process.env.URLDB = urlDB;
